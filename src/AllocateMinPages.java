68ebb2d67dc41d92c5523878e0484a9fd76a64c5
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-07-12 23:32:02 -0700

    added two methods of check and AllocateMinPagesto check the max number of minimum pages allocated to a student

--- src/AllocateMinPages.java
+++ src/AllocateMinPages.java
@@ -1,43 +1,52 @@
-/*You are given N number of books. Every ith book has Ai number of pages. The books are arranged in ascending order.
-
-You have to allocate contiguous books to M number of students. There can be many ways or permutations to do so. In each permutation, one of the M students will be allocated the maximum number of pages. Out of all these permutations, the task is to find that particular permutation in which the maximum number of pages allocated to a student is the minimum of those in all the other permutations and print this minimum value.
-
-Each book will be allocated to exactly one student. Each student has to be allocated at least one book.
-
-Note: Return -1 if a valid assignment is not possible, and allotment should be in contiguous order (see the explanation for better understanding).
-
-
-
-Example 1:
-
-Input:
-N = 4
-A[] = {12,34,67,90}
-M = 2
-Output:113
-Explanation:Allocation can be done in
-following ways:{12} and {34, 67, 90}
-Maximum Pages = 191{12, 34} and {67, 90}
-Maximum Pages = 157{12, 34, 67} and {90}
-Maximum Pages =113. Therefore, the minimum
-of these cases is 113, which is selected
-as the output.
-
-Example 2:
-
-Input:
-N = 3
-A[] = {15,17,20}
-M = 2
-Output:32
-Explanation: Allocation is done as
-{15,17} and {20} */
-
-
-
-
-
-
 public class AllocateMinPages {
 
+    static boolean check(int[] arr,int n, int m, int mid){
+        int count = 0;
+        int pages = 0;
+        for(int i= 0; i<n; i++){
+            if(arr[i] + pages <= mid){
+                pages += arr[i];
+            }else{
+                count ++;
+                pages = arr[i];
+            }
+        }
+        if(count<=m){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+
+
+    public static int allocateMinPages(int [] arr, int n, int m){
+        int low = 0;
+        int high = 0;
+        for(int i=0; i<n; i++){
+            low = Math.max(low, arr[i]);
+            high += arr[i];
+        }
+        int ans = -1;
+        while(low<=high){
+            int mid = (low+ high)/2;
+            if((check(arr, n, m, mid))){
+                high = mid-1;
+                ans = mid;
+            }else{
+                low = mid-1;
+            }
+        }return ans;
+
+
+    }
+
+
+
+
+
+
+    public static void main(String[] args) {
+
+    }
 }
