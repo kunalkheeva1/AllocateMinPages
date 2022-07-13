50b6a60267e9edbde697aa1e2a7eeae9a5a41320
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-07-12 23:37:15 -0700

    added comments

--- src/AllocateMinPages.java
+++ src/AllocateMinPages.java
@@ -1,16 +1,22 @@
 public class AllocateMinPages {
 
     static boolean check(int[] arr,int n, int m, int mid){
-        int count = 0;
+        // assume n initially we have atleast one student and 0 pages to read
+        int count = 1;
         int pages = 0;
+
+
         for(int i= 0; i<n; i++){
+            // pages increase wrt mid
             if(arr[i] + pages <= mid){
                 pages += arr[i];
+                // else student increases and reads another book
             }else{
                 count ++;
                 pages = arr[i];
             }
         }
+        // and return true and false accordingly
         if(count<=m){
             return true;
         }else{
@@ -21,20 +27,28 @@ public class AllocateMinPages {
 
 
     public static int allocateMinPages(int [] arr, int n, int m){
+
+
         int low = 0;
         int high = 0;
+
+        //low is if there are n students and read n books, and the max page count is stored in low
+        // High stores if one student reads all the books
         for(int i=0; i<n; i++){
             low = Math.max(low, arr[i]);
             high += arr[i];
         }
         int ans = -1;
+        // our ans is contained in low to high
         while(low<=high){
             int mid = (low+ high)/2;
+            // so if mid is the ans then check for another mid on LHS
             if((check(arr, n, m, mid))){
                 high = mid-1;
                 ans = mid;
+                // else check the other half of the low - high
             }else{
-                low = mid-1;
+                low = mid+1;
             }
         }return ans;
 
